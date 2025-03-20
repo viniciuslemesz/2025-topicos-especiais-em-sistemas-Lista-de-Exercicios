2af57ecf1fe4a840c237bd98e0219d30449e0e21
VINICIUS LEMES DOMINGUES <vinicius.lemes@cs.up.edu.br>
2025-03-20 09:22:20 -0300

    Correção da Lista de Exercicios

---
+++ ListaExercicios/Exe06a/Program.cs
@@ -0,0 +1,50 @@
+﻿// Criar vetor de inteiros
+int[] vetor = new int[10];
+
+// Criar um gerador de números aleatórios
+Random r = new Random();
+
+// Exibir um número aleatório para teste
+Console.WriteLine(r.Next(1000));
+Console.Write("\n");
+// Preencher o vetor com números aleatórios
+for (int i = 0; i < vetor.Length; i++)
+ {
+    vetor[i] = r.Next(1000);
+    
+ }
+
+// Exibir o vetor original
+for (int i = 0; i < vetor.Length; i++)
+{
+  Console.Write(vetor[i] + " ");
+}
+
+Console.WriteLine("\n");
+
+// Ordenar o vetor usando Bubble Sort
+bool troca;
+    do
+    {
+      troca = false;
+      for (int i = 0; i < vetor.Length - 1; i++)
+    {
+        // Trocar valores se necessário
+        if (vetor[i] > vetor[i + 1]) 
+         {
+        int aux = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = aux;
+        troca = true;
+        }
+     }
+        } while (troca == true);
+
+        // Exibir o vetor ordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+
+        Console.WriteLine("\n");
+  
---
+++ ListaExercicios/Exe06b/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        // Criar vetor de inteiros
+        int[] vetor = new int[10];
+
+        // Criar um gerador de números aleatórios
+        Random r = new Random();
+
+        // Exibir um número aleatório para teste
+        Console.WriteLine(r.Next(1000));
+        Console.Write("\n");
+        
+        // Preencher o vetor com números aleatórios
+        for (int i = 0; i < vetor.Length; i++)
+        {
+        vetor[i] = r.Next(1000);
+    
+        }
+
+        // Exibir o vetor original
+        for (int i = 0; i < vetor.Length; i++)
+        {
+        Console.Write(vetor[i] + " ");
+        }
+
+        Array.Sort(vetor);
+
+        Console.WriteLine("\n");
+        // Exibir o vetor ordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+        Console.Write(vetor[i] + " ");
+        }
+
+        Console.WriteLine("\n");
+  
+    }
+
+}
